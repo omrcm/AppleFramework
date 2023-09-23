e5d094010fb81b8e828f981d429fd787a8d386a6
omrcm <omurucum@gmail.com>
2023-09-23 03:05:44 +0300

    First Commit

---
+++ Apple Framework/Apple_FrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworkApp.swift
+//  Apple Framework
+//
+//  Created by Ömür Uçum on 23.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple Framework/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Apple Framework
+//
+//  Created by Ömür Uçum on 23.09.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world! asd")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
